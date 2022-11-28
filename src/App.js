ddda9ffabe2e54c174fc894f67f833173a826438
Maikuri Emma <50575460+Emmabsy@users.noreply.github.com>
2022-11-28 11:50:00 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import 'antd/dist/reset.css';
 import Todo from './pages/Todo';
 import Navbar from './components/Navbar';
-
+import Sub from './components/Sub';
 
 function App() {
   return (
